6f9490dabdb69b7ceddff33fa97a6c179fb2c8f9
Ben McMorran <ben.mcmorran@gmail.com>
2016-11-04 13:43:03 -0400

    Working camera preview and OpenCV binaries

--- app/src/main/java/edu/wpi/hexahedron/MainActivity.java
+++ app/src/main/java/edu/wpi/hexahedron/MainActivity.java
@@ -1,13 +1,25 @@
 package edu.wpi.hexahedron;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends Activity {
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.e("Main", "Couldn't load OpenCV");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
